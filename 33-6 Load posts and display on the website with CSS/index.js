dc615d645f82e949147106a2de502a56546f462c
S M Armaan Hossain <armaanhossainirtaza7@gmail.com>
2023-05-10 22:57:27 +0600

    js file added

---
+++ 33-6 Load posts and display on the website with CSS/index.js
@@ -0,0 +1,26 @@
+function loadPost() {
+  fetch("https://jsonplaceholder.typicode.com/posts")
+    .then((res) => res.json())
+    .then((data) => displayPost(data));
+}
+
+function displayPost(posts) {
+  const postContainer = document.getElementById("post-container");
+
+  for (const post of posts) {
+    const div = document.createElement("div");
+
+    div.classList.add("post");
+    div.innerHTML = `
+    
+    <h4>User - ${post.userId} </h4>
+    <h5>Post :  ${post.title}</h5>
+    <p>Post Description : ${post.body} </p>
+    
+    `;
+
+    postContainer.appendChild(div);
+  }
+}
+
+loadPost();
